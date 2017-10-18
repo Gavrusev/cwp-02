f9fc92e8b01ea2855abe40dbb6c531d22aaea1d5
Aleksey Gavrusev <lexa1907_98@mail.ru>
2017-10-18 09:42:48 +0300

    task 8

--- client.js
+++ client.js
@@ -1,78 +1,73 @@
 const net = require('net');
-const fs = require('fs');
 const port = 8124;
 const startConnect = 'QA';
-const serverOK="ASK";
-let questions=[];
+const serverOK = 'ACK';
+let questions = [];
 const serverNO = 'DEC';
-let  currentQuestionIndex = 0;
+let currentQuestionIndex = 0;
 const qaPath = "./qa.json";
-const qaPath = "./qa.json";
-
 const client = new net.Socket();
 const fs = require('fs');
 let ind = 0;
 client.setEncoding('utf8');
- client.on('data',AskQuestions);
+
 
 client.connect(port, function() {
 
-
-  fs.readFile(qaPath, function (err, data) {
-           if (err) {
-               console.log(err);
-           }
-           else {
-               questions = JSON.parse(data);
-               mixQuestion();
-              console.log('Connected');
-     client.write(startConnect);
-           }
-       });
+    fs.readFile(qaPath, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            questions = JSON.parse(data);
+            mixQuestion();
+           console.log('Connected');
+  client.write(startConnect);
+        }
+    });
 });
 
 
 
 client.on('data', function(data) {
+    if (data === serverOK)
+    {
+        client.write(questions[ind].question);
 
-     if (data === servrOK)
-         {
-             client.write(questions[ind].quesion);
-    
+    }
+    else if (data === serverNO)
+    {
+        console.log(data);
+        client.destroy();
 
-
-     if (data === serverOK)
-        {
-             client.write(questions[ind].question);
-
+    }
+    else if (data !== serverOK) {
+        console.log("\nQuestion: " + questions[ind].question);
+        console.log("Server answer: " + data);
+        console.log(data === questions[ind].true.toString() ?
+            "Server answer is true" :
+            "True answer: " + questions[ind].true);
+        if (++ind !== questions.length) {
+            client.write(questions[ind].question)
         }
-        else
-      {
-           questions = JSON.parse(data);
-            mixQuestion();
-            client.write(serverOK);
-
-         for (let i = 0 ; i < questions.length; i ++)
-              {
-                console.log(questions[i].question);
-                console.log(questions[i].true);
-                console.log(questions[i].false);
-              }
-
+        else {
+            client.destroy();
         }
-    });
-}
+    }
 
-else
-{
-  client.destroy();
-}
-  });
+});
+
+client.on('close', function() {
+  console.log('Connection closed');
+});
+
+
 function mixQuestion() {
     let counter = questions.length;
 
     while (counter > 0) {
         let index = Math.floor(Math.random() * counter);
+
         counter--;
 
         let temp = questions[counter];
--- server.js
+++ server.js
@@ -1,49 +1,117 @@
-// server.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
-let seed = 0;
 const serverOK = 'ACK';
 const serverNO = 'DEC';
 const startConnect = 'QA';
-const qaPath = "./qa.json";
-const clientLogPathDefault = './logs'
+const qaPath = "E://PSCP//lr3//qa.json";
+const logFile = "E://PSCP//lr3//logs";
 let questions = [];
 let seed = 0;
-let fdFile;
-   const server = net.createServer((client) => {
+
+const server = net.createServer((client) => {
 
 
-         client.setEncoding('utf8');
+    client.setEncoding('utf8');
 
-   сlient.on('data',UserDialog);
+client.on('data',UserDialog);
+client.on('data',AskQuestions);
 
 
-           function UserDialog(data, err)
+function UserDialog(data, err)
+{
+    if (!err)
     {
-        if (!err)
-            {
-               if (data === startConnect)
-               {
-                   client.id = getUniqId();
-                client.write(serverOK);
-                console.log(client.id + "  connected");
-            }
+        if (data === startConnect)
+        {
+            client.id = getUniqId();
+
+            fs.writeFile(client.id+`.txt`,`Client ${client.id} is connect\r\n`,(err)=>{if(err) console.log('Error');});
+
+
+
+
+            console.log(client.id + "  connected");
+            client.write(serverOK);
+
+
         }
-         else
-         {
-            client.write(serverNO);
-            client.write(err);
-             client.on('end', () => console.log('Client disconnected'));
-           }
     }
+    else
+    {
+        client.write(serverNO);
+        client.write(err);
+    }
+}
 
-     });
-  server.listen(port, () => {
-  console.log("Server listening on localhost: " + port);
-});
 
 
-       function getUniqId()
+
+
+
+function AskQuestions(data, err) {
+    if (!err) {
+        if (data !== startConnect) {
+            let questionObj = getQuestionObj(data);
+            let serverAnswer = questionObj[(Math.random() < 0.5) ? "true" : "false"].toString();
+
+
+            fs.appendFile(client.id + '.txt','Q: ' + questionObj.question + '\n', function(error)
+            {
+
+                if(error) throw error;
+
+
+            });
+
+            fs.appendFile(client.id+ '.txt','A: ' + serverAnswer+ '\n', function(error)
+            {
+
+                if(error) throw error;
+
+
+            });
+
+
+
+            client.write(serverAnswer);
+        }
+    }
+    else
+    {
+        console.log(err);
+    }
+}
+
+
+function getQuestionObj(question) {
+    for (let i = 0; i < questions.length; i++) {
+        if (questions[i].question === question) {
+            return questions[i];
+        }
+    }
+}
+
+
+function getUniqId()
 {
     return seed++;
 }
+
+
+});
+
+
+server.listen(port, () => {
+    console.log("Server listening on localhost: " + port);
+
+fs.readFile(qaPath, function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        questions = JSON.parse(data);
+    }
+});
+
+});
