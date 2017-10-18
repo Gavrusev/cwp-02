c734af5061e262b2b3855ed31f6ac62f1cd22e8e
Aleksey Gavrusev <lexa1907_98@mail.ru>
2017-10-18 09:01:21 +0300

    task 1-3

--- client.js
+++ client.js
@@ -1,21 +1,20 @@
-// client.js
 const net = require('net');
 const port = 8124;
+const startConnect = 'QA';
 
 const client = new net.Socket();
 
 client.setEncoding('utf8');
 
+
 client.connect(port, function() {
-    console.log('Connected');
-    client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+  console.log('Connected');
+  client.write(startConnect);
 });
+
+
 
 client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
-});
-
-client.on('close', function() {
-    console.log('Connection closed');
+  console.log(data);
+  client.destroy();
 });
--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 // server.js
 const net = require('net');
 const port = 8124;
+let seed = 0;
+const serverOK = 'ACK';
+const serverNO = 'DEC';
+const startConnect = 'QA';
 
-const server = net.createServer((client) => {
-    console.log('Client connected');
+   const server = net.createServer((client) => {
 
-client.setEncoding('utf8');
 
-client.on('data', (data) => {
-    console.log(data);
-client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+         client.setEncoding('utf8');
+
+   сlient.on('data',UserDialog);
+
+
+           function UserDialog(data, err)
+    {
+        if (!err)
+            {
+               if (data === startConnect)
+               {
+                   client.id = getUniqId();
+                client.write(serverOK);
+                console.log(client.id + "  connected");
+            }
+        }
+         else
+         {
+            client.write(serverNO);
+            client.write(err);
+
+           }
+    }
+
+     });
+  server.listen(port, () => {
+  console.log("Server listening on localhost: " + port);
 });
 
-client.on('end', () => console.log('Client disconnected'));
-});
 
-server.listen(port, () => {
-    console.log(`Server listening on localhost:${port}`);
-});
+       function getUniqId()
+{
+    return seed++;
+}
